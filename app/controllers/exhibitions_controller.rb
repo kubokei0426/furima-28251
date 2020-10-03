d0823074b3b975a39445928b7aa29cd61e700cbb
kubokei0426 <kubokei0426@users.noreply.github.com>
2020-10-03 17:59:14 +0900

    ActiveHashの追加

--- app/controllers/exhibitions_controller.rb
+++ app/controllers/exhibitions_controller.rb
@@ -4,4 +4,15 @@ class ExhibitionsController < ApplicationController
   def new
     @exhibitions = Exhibition.new
   end
+  def create
+    Exhibition.create(exhibition_params)
+  end
+
+  private
+  def exhibition_params
+    params.require(:exhibition).permit(
+      :image, :name, :text, :price, :category_id,
+      :status_id, :fee_id, :prefecture_id, :delivery_id, :user_id)
+  end
+
 end
--- app/models/exhibition.rb
+++ app/models/exhibition.rb
@@ -1,2 +1,12 @@
 class Exhibition < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :fee
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :delivery
+
+  validates :image, :name, :text, :price, :category, :status, :fee, :prefecture, :delivery, presence: true
+  validates :image, :name, :text, :price, :category, :status, :fee, :prefecture, :delivery, numericality: { other_than: 1 } 
+
 end
