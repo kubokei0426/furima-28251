cf3edf5b61dee1125db60938146d321346b17cda
kubokei0426 <kubokei0426@users.noreply.github.com>
2020-10-09 11:26:54 +0900

    単体テストコードの追加

---
+++ spec/factories/exhibitions.rb
@@ -0,0 +1,17 @@
+FactoryBot.define do
+  factory :exhibition do
+    name {Faker::Lorem.sentence}
+    text {Faker::Lorem.sentence}
+    category_id            { 2 }
+    status_id            { 2 }
+    fee_id            { 2 }
+    prefecture_id            { 2 }
+    delivery_id            { 2 }
+    price                  { 300 }
+    association :user 
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+  end
+end
---
+++ spec/models/exhibition_spec.rb
@@ -0,0 +1,67 @@
+require 'rails_helper'
+
+RSpec.describe Exhibition, type: :model do
+  before do
+    @exhibition = FactoryBot.build(:exhibition)
+  end
+
+  describe '出品の保存' do
+    context "出品が保存できる場合" do
+      it "出品画像と商品名と商品の説明とカテゴリーと商品の状態と配送料の負担と発送元の地域と発送までの日数と販売価格があれば出品は保存される" do
+        expect(@exhibition).to be_valid
+      end
+    end
+    context "出品が保存できない場合" do
+      it "出品画像がないと出品は保存できない" do
+        @exhibition.image = nil
+        @exhibition.valid?
+        expect(@exhibition.errors.full_messages).to include("画像を入力してください")
+      end     
+      it "商品名がないと出品は保存できない" do
+        @exhibition.name = nil
+        @exhibition.valid?
+        expect(@exhibition.errors.full_messages).to include("商品名を入力してください")
+      end 
+      it "商品説明がないと出品は保存できない" do
+        @exhibition.text = nil
+        @exhibition.valid?
+        expect(@exhibition.errors.full_messages).to include("商品説明を入力してください")
+      end 
+      it "カテゴリーがないと出品は保存できない" do
+        @exhibition.category_id = nil
+        @exhibition.valid?
+        expect(@exhibition.errors.full_messages).to include("カテゴリーを入力してください")
+      end 
+      it "商品状態がないと出品は保存できない" do
+        @exhibition.status_id = nil
+        @exhibition.valid?
+        expect(@exhibition.errors.full_messages).to include("商品状態を入力してください")
+      end 
+      it "配送料がないと出品は保存できない" do
+        @exhibition.fee_id = nil
+        @exhibition.valid?
+        expect(@exhibition.errors.full_messages).to include("配送料を入力してください")
+      end      
+      it "発送元地域がないと出品は保存できない" do
+        @exhibition.prefecture_id = nil
+        @exhibition.valid?
+        expect(@exhibition.errors.full_messages).to include("発送元地域を入力してください")
+      end 
+      it "発送日数がないと出品は保存できない" do
+        @exhibition.delivery_id = nil
+        @exhibition.valid?
+        expect(@exhibition.errors.full_messages).to include("発送日数を入力してください")
+      end 
+      it "販売価格がないと出品は保存できない" do
+        @exhibition.price = nil
+        @exhibition.valid?
+        expect(@exhibition.errors.full_messages).to include("販売価格を入力してください")
+      end 
+      it "ユーザーが紐付いていないと出品は保存できない" do
+        @exhibition.user = nil
+        @exhibition.valid?
+        expect(@exhibition.errors.full_messages).to include("ユーザーを入力してください")
+      end
+    end
+  end
+end
