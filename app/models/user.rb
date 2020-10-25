57fb0f2cd4bca43639d7494646713ddc3b2a198d
kubokei0426 <kubokei0426@users.noreply.github.com>
2020-10-25 13:04:21 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,7 @@ class User < ApplicationRecord
   validates :first_name, format: { with:
                                   VALID_NAME_REGEX}
   validates :last_name, format: { with:
-                                  VALID_NAME_KANA_REGEX}
+                                  VALID_NAME_REGEX}
   validates :first_name_kana, format: { with:
                                       VALID_NAME_KANA_REGEX}
   validates :last_name_kana, format: { with:
