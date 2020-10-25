217e70418f9c3f0c8a7dcf4eef8b3528870de837
kubokei0426 <kubokei0426@users.noreply.github.com>
2020-10-25 13:02:26 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,20 +6,24 @@ class User < ApplicationRecord
   has_many :exhibitions
   has_many :purchasers
   with_options presence: true do
-    VALID_PASSWORD_REGEX = /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]{6,}+\z/.freeze
-    VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/.freeze
-    VALID_NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/.freeze
-    validates :nickname
-    validates :password, format: { with:VALID_PASSWORD_REGEX}
-    validates :password_confirmation
-    validates :first_name, format: { with:
-                        VALID_NAME_REGEX}
-    validates :last_name, format: { with:
-                       VALID_NAME_REGEX}
-    validates :first_name_kana, format: { with:
-                       VALID_NAME_KANA_REGEX}
-    validates :last_name_kana, format: { with:
-                       VALID_NAME_KANA_REGEX}
-    validates :birth_date
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]{6,}+\z/.freeze
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/.freeze
+  VALID_NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/.freeze
+  validates :nickname
+  validates :password, format: { with:VALID_PASSWORD_REGEX}
+  validates :password_confirmation
+  validates :email
+  validates :password, length: { minimum: 6 },
+                                format: { with:
+                                VALID_PASSWORD_REGEX}
+  validates :first_name, format: { with:
+                                  VALID_NAME_REGEX}
+  validates :last_name, format: { with:
+                                  VALID_NAME_KANA_REGEX}
+  validates :first_name_kana, format: { with:
+                                      VALID_NAME_KANA_REGEX}
+  validates :last_name_kana, format: { with:
+                                      VALID_NAME_KANA_REGEX}
+  validates :birth_date
   end
 end
